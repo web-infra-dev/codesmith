2ee972b4ed5188338b89f4f1ebab5db08fdb2ddf
caohuilin <caohuilin@bytedance.com>
2021-10-21 18:10:40 +0800

    fix: test

--- packages/core/tests/utils/downloadPackage.test.ts
+++ packages/core/tests/utils/downloadPackage.test.ts
@@ -1,6 +1,6 @@
 import { downloadPackage } from '@/utils';
 
-describe.skip('downloadPackage function test', () => {
+describe('downloadPackage function test', () => {
   it('download lodash package', async () => {
     const packageDir = await downloadPackage('lodash');
     expect(packageDir).toContain('csmith-generator/lodash');
