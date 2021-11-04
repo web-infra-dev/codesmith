d4442a96201418e26767f578c883a38141989aaa
caohuilin <caohuilin@bytedance.com>
2021-11-04 19:31:33 +0800

    fix: test

--- packages/core/tests/utils/downloadPackage.test.ts
+++ packages/core/tests/utils/downloadPackage.test.ts
@@ -1,6 +1,12 @@
+import path from 'path';
+import os from 'os';
+import fs from 'fs-extra';
 import { downloadPackage } from '@/utils';
 
 describe('downloadPackage function test', () => {
+  beforeEach(() => {
+    fs.removeSync(path.join(os.tmpdir(), 'csmith-generator'));
+  });
   it('download lodash package', async () => {
     const packageDir = await downloadPackage('lodash');
     expect(packageDir).toContain('csmith-generator/lodash');
