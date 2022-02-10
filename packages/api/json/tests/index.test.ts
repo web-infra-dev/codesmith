547987f573debf64c3950b6d280e54a73d25f1fc
Belinda Cao <caohuilin@bytedance.com>
2022-02-10 14:19:44 +0800

    feat: use modern 1.0.0 version (#29)

---
+++ packages/api/json/tests/index.test.ts
@@ -0,0 +1,99 @@
+import commentJSON from 'comment-json';
+import * as declarationUpdate from 'declaration-update';
+
+describe('update json', () => {
+  it('simple string', () => {
+    const jsonContent = commentJSON.parse('{}');
+    declarationUpdate.query(
+      jsonContent,
+      {},
+      {
+        $set: { a: '1' },
+      },
+    );
+    expect(jsonContent).toEqual({ a: "1" });
+  });
+  it('complex string', () => {
+    const jsonContent = commentJSON.parse('{}');
+    declarationUpdate.query(
+      jsonContent,
+      {},
+      {
+        $set: {
+          a: {
+            b: "2"
+          }
+        },
+      },
+    );
+    expect(jsonContent).toEqual({
+      a: {
+        b: "2"
+      }
+    });
+  });
+  it('simple array', () => {
+    const jsonContent = commentJSON.parse('{}');
+    declarationUpdate.query(
+      jsonContent,
+      {},
+      {
+        $set: [
+          "1", "2", "3"
+        ]
+      },
+    );
+    expect(jsonContent).toEqual({
+       "0": "1",
+       "1": "2",
+       "2": "3",
+    });
+  });
+  it('complex array', () => {
+    const jsonContent = commentJSON.parse('{}');
+    declarationUpdate.query(
+      jsonContent,
+      {},
+      {
+        $set: [{
+          a: "1"
+        }, {
+          b: "2"
+        }, {
+          c: "3"
+        }]
+      },
+    );
+    expect(jsonContent).toEqual({
+        "0": {
+           "a": "1",
+         },
+         "1": {
+           "b": "2",
+         },
+         "2": {
+           "c": "3",
+         }});
+  });
+  it('string and array', () => {
+    const jsonContent = commentJSON.parse('{}');
+    declarationUpdate.query(
+      jsonContent,
+      {},
+      {
+        $set: {
+          a: ["1", "2", "3"],
+          b: {
+            "b1": "b1"
+          }
+        }
+      },
+    );
+    expect(jsonContent).toEqual({
+      a: ["1", "2", "3"],
+      b: {
+        "b1": "b1"
+      }
+    });
+  });
+});
