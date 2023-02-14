31c28060ee676cd93804e5d54396e66c5f1ce741
caohuilin <caohuilin@bytedance.com>
2023-02-14 14:31:33 +0800

    feat: registry monorepo tools and test plugin

--- packages/core/modern.config.ts
+++ packages/core/modern.config.ts
@@ -1,7 +1,8 @@
 import moduleTools, { defineConfig } from '@modern-js/module-tools';
+import testPlugin from '@modern-js/plugin-testing';
 import { nodeBuildConfig } from '@modern-js/build-config';
 
 export default defineConfig({
   buildConfig: nodeBuildConfig,
-  plugins: [moduleTools()],
+  plugins: [moduleTools(), testPlugin()],
 });
