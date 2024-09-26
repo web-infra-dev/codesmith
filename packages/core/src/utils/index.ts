5d8979458406de9626967e93ef281bd83b7df6b6
caohuilin <caohuilin@bytedance.com>
2024-09-26 16:48:11 +0800

    feat: delete get npm tarball url function

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -3,6 +3,5 @@ export { nodeRequire } from './nodeRequire';
 export { canUsePnpm, canUseYarn, runInstall } from './packageManager';
 export { timeoutPromise } from './timeoutPromise';
 export { downloadPackage } from './downloadPackage';
-export { getNpmTarballUrl } from './getNpmTarballUrl';
 export { getNpmVersion } from './getNpmVersion';
 export { getPackageInfo } from './getPackageInfo';
