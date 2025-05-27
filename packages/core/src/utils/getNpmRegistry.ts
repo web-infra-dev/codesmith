d16d7299701baac3978a8c9e1945e255347dcecc
caohuilin <caohuilin@bytedance.com>
2025-05-27 13:26:30 +0800

    feat: download package support use npm command

--- packages/core/src/utils/getNpmRegistry.ts
+++ packages/core/src/utils/getNpmRegistry.ts
@@ -1,6 +1,16 @@
 import { execa } from '@modern-js/codesmith-utils/execa';
 
 export async function getNpmRegistry() {
-  const { stdout } = await execa('npm', ['config', 'get', 'registry']);
-  return stdout.replace(/\/$/, '') || 'https://registry.npmjs.org';
+  try {
+    const { stdout } = await execa('npm', [
+      'config',
+      'get',
+      'registry',
+      '--no-workspaces',
+    ]);
+    return stdout.replace(/\/$/, '') || 'https://registry.npmjs.org';
+  } catch (error) {
+    // If getting registry fails, return default npm registry
+    return 'https://registry.npmjs.org';
+  }
 }
